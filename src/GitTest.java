ef90be98f3bb73715c8faf27e0bdd74893e674ef
Chopita <chopa1404@gmail.com>
2021-03-16 18:56:14 -0500

    naza added

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("100_000.00 $");
         System.out.println("100");
         System.out.println("Cholpon");
+        System.out.println("Naza");
     }
 }
